a770c4a2003808d17c93b59ebd7acd4ee036250f
Ljazz <elabdrasulov@gmail.com>
2022-06-30 13:11:00 +0600

    crud

--- function-based-crud/crud.py
+++ function-based-crud/crud.py
@@ -1,165 +1,95 @@
-"==========CRUD============"
+"============CRUD=============="
 # Create - создать
 # Read - прочитать
-# Update - обновить 
+# Update - обновить
 # Delete - удалить
 
-from utils import validate_id
-
-# database = {
-#     "Бекзат": "скала",
-#     "Эртай": "пароль",
-#     "Оомат": "Кыргызстан",
-#     "Имран": "12345",
-#     "Жийде": "return",
-#     "Манас": "Маке",
-#     "Арафат": "54321",
-#     "Элжаз": "парол",
-#     "Гулсана": "312",
-#     "Эркайым": "Айдин",
-#     "Бекназ": "Арёль",
-#     "Эдиль": "ьлорап",
-#     "Айгул": "май",
-#     "Закир": "@@@",
-#     "Бегайым": "makers",
-#     "Мырзайым": "Bootcamp2221",
-#     "Даниэл": "covid19",
-#     "Жибек": "1404",
-#     "Калысбек": "стол",
-#     "Ырыс": "suuuuuuuuiiiiiiiiiiii",
-#     "Айканыш": "qwerty",
-#     "Арген": "11172332",
-#     "Нурмухамед": "Не верный",
-#     "Бектур": "0101",
-#     "Алан": "душу питона",
-#     "Куба": "1",
-#     "Жаангер": "ох блин",
-#     "Богдан": "Кудайберген",
-#     "Айгерим": "синий маркер",
-#     "Настя": "Python21",
-#     "Жаркынай": "Мафия"
-# }
-
-# import random
+from utils import validate_id, read_db, write_to_db
 
 
-# ids = []
-
-# for key, value in database.copy().items():
-#     id = random.randint(100, 999)
-#     while id in ids:
-#         id = random.randint(100, 999)
-#     ids.append(id)
-
-#     database[id] = {
-#         'name': key,
-#         'password': value,
-#         'info': '...'
-#     }
-#     del database[key]
-
-# print(database)
-
-database = {
-    296: {'name': 'Бекзат', 'password': 'скала', 'info': '...'}, 
-    134: {'name': 'Эртай', 'password': 'пароль', 'info': '...'}, 
-    987: {'name': 'Оомат', 'password': 'Кыргызстан', 'info': '...'}, 
-    273: {'name': 'Имран', 'password': '12345', 'info': '...'}, 
-    596: {'name': 'Жийде', 'password': 'return', 'info': '...'}, 
-    514: {'name': 'Манас', 'password': 'Маке', 'info': '...'}, 
-    912: {'name': 'Арафат', 'password': '54321', 'info': '...'}, 
-    801: {'name': 'Элжаз', 'password': 'парол', 'info': '...'}, 
-    518: {'name': 'Гулсана', 'password': '312', 'info': '...'}, 
-    366: {'name': 'Эркайым', 'password': 'Айдин', 'info': '...'}, 
-    861: {'name': 'Бекназ', 'password': 'Арёль', 'info': '...'}, 
-    599: {'name': 'Эдиль', 'password': 'ьлорап', 'info': '...'}, 
-    567: {'name': 'Айгул', 'password': 'май', 'info': '...'}, 
-    394: {'name': 'Закир', 'password': '@@@', 'info': '...'}, 
-    672: {'name': 'Бегайым', 'password': 'makers', 'info': '...'}, 
-    182: {'name': 'Мырзайым', 'password': 'Bootcamp2221', 'info': '...'}, 
-    770: {'name': 'Даниэл', 'password': 'covid19', 'info': '...'}, 
-    420: {'name': 'Жибек', 'password': '1404', 'info': '...'}, 
-    556: {'name': 'Калысбек', 'password': 'стол', 'info': '...'}, 
-    570: {'name': 'Ырыс', 'password': 'suuuuuuuuiiiiiiiiiiii', 'info': '...'}, 
-    954: {'name': 'Айканыш', 'password': 'qwerty', 'info': '...'}, 
-    149: {'name': 'Арген', 'password': '11172332', 'info': '...'}, 
-    267: {'name': 'Нурмухамед', 'password': 'Не верный', 'info': '...'}, 
-    209: {'name': 'Бектур', 'password': '0101', 'info': '...'}, 
-    731: {'name': 'Алан', 'password': 'душу питона', 'info': '...'}, 
-    718: {'name': 'Куба', 'password': '1', 'info': '...'}, 
-    653: {'name': 'Жаангер', 'password': 'ох блин', 'info': '...'}, 
-    405: {'name': 'Богдан', 'password': 'Кудайберген', 'info': '...'}, 
-    698: {'name': 'Айгерим', 'password': 'синий маркер', 'info': '...'}, 
-    744: {'name': 'Настя', 'password': 'Python21', 'info': '...'}, 
-    689: {'name': 'Жаркынай', 'password': 'Мафия', 'info': '...'}
-    }
+database = read_db("db.json")
 
 
-def read(u_id):
+def read(u_id, update=False):
     """
-    Принимает id юзера\n
-    Выводит его имя и информацию\n
-    Если такого юзера нет, вызывается Exception
+    Принимает id юзера
+    Выводит его имя и информацию
+    Если такого юзера нет вызывается Exception
     """
     validate_id(database.keys(), u_id)
-    name = database[u_id]['name']
-    info = database[u_id]['info']
-    print(f"""
-==========={u_id}============
-Name: {name}
-Info: {info}
-==========================
-""")
+    name = database[u_id]["name"]
+    info = database[u_id]["info"]
+    sex = database[u_id]["sex"]
+    if update:
+        print(f"""
+    ===========Update user: {u_id}============
+    Name: {name}
+    Sex: {sex}
+    Info: {info}
+    ==========================
+    """)
+    else:
+        print(f"""
+    ==========={u_id}============
+    Name: {name}
+    Sex: {sex}
+    Info: {info}
+    ==========================
+    """)
+
 
 
 def create():
     """
-    Запрашивает данные о пользователе\n
-    Записывает их в базу данных
+    Запрашивает данные о пользователе
+    Записывает в бд
     """
     from utils import generate_id, validate_passwords
-    name = input('Введите имя: ')
+    name = input("Введите имя: ")
     password = input("Введите пароль: ")
     password2 = input("Введите подтверждение пароля: ")
     validate_passwords(password, password2)
     info = input("Введите информацию о вас: ")
+    sex = input("Укажите пол (м,ж): ")
     id_ = generate_id(database.keys())
     database[id_] = {
-        "name": name, 
+        "name": name,
         "password": password,
-        "info": info
+        "info": info,
+        "sex": sex
     }
+    print("Вы были успешно добавлены в Python21")
+    write_to_db("db.json", database)
+    return id_
 
-    print("Вы были успещно добавлены в Python21")
-    
 
 def delete(u_id):
     """
-    Принимает id пользователя\n
-    Если юзер существует, он удаляется из базы данных\n
-    Если юзера нет, то вызывается ошибка
+    Принимает id пользователя
+    Если юзер существует, он удаляется из базы данных
+    Если юзера нет, вызывается ошибка
     """
     validate_id(database.keys(), u_id)
-    name = database[u_id]['name']
+    name = database[u_id]["name"]
+    sex = database[u_id]['sex']
     del database[u_id]
-    print(f'user {name} был успешно удален')
-
+    if sex == 'м':
+        print(f"{name} был успешно удален")
+    else:
+        print(f"{name} была успешно удалена")
+    write_to_db("db.json", database)
 
 def update(u_id):
     """
-    Принимает id юзера\n
-    Выводит старые данные\n
-    Принимает новые данные\n
+    Принимает id юзера
+    Выводит старые данные
+    Принимает новые данные
     Изменяет в базе данных
     """
     validate_id(database.keys(), u_id)
-    read(u_id)
-    #  принимает новые данные
-    name = input("Введите имя: ")
+    read(u_id, update=True)
+    # принимаем новые данные
     info = input("Введите информацию о вас: ")
-    # sex = input("Укажите пол (м,ж): ")
-
-    database[u_id]['name'] = name
-    database[u_id]['info'] = info
-    # database[u_id]['sex'] = sex
-    read(u_id)
+    # изменяем в бд
+    database[u_id]["info"] = info
+    write_to_db("db.json", database)
--- function-based-crud/utils.py
+++ function-based-crud/utils.py
@@ -3,18 +3,18 @@
 
 def generate_id(ids):
     """
-    Принимает список существующих id\n
+    Принимает список существующих id
     Возвращает новое id в диапазоне от 100 до 1000
     """
     import random
     id_ = random.randint(100, 1000)
     while id_ in ids:
         id_ = random.randint(100, 1000)
-    return id_
+    return str(id_)
 
 def validate_passwords(p1, p2):
     """
-    Принимает 2 пароля\n
+    Принимает 2 пароля
     Если они не совпадают, вызывается ошибка
     """
     if p1 != p2:
@@ -22,8 +22,28 @@ def validate_passwords(p1, p2):
 
 def validate_id(ids, u_id):
     """
-    Принимает список существующих id и id, которое нужно проверить\n
+    Принимает список существующих id и id, которое нужно проверить
     Если такого id нет в списке, вызывается Exception
     """
     if u_id not in ids:
         raise Exception("Такого юзера нет")
+
+
+def read_db(name):
+    """
+    Принимает название файла
+    Возвращает данные из бд в виде python обьектов
+    """
+    import json
+    with open(name, "r") as file:
+        db = json.load(file)
+    return db
+
+def write_to_db(name, data):
+    """
+    Принимает название файла и данные для записи
+    Записывает эти данные в файл
+    """
+    import json
+    with open(name, "w", encoding='utf8') as file:
+        json.dump(data, file, ensure_ascii=False)
