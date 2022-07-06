d86afe284a74a029e4f4190ffe992ba47231efd8
Ljazz <elabdrasulov@gmail.com>
2022-07-06 12:55:01 +0600

    views

--- oop-shop/main.py
+++ oop-shop/main.py
@@ -1,11 +1,19 @@
 from shop.models import Product, Category
-from shop.serializers import ProductSerializer
+from shop.views import product_create, product_delete, product_detail, product_list, product_update
 
 cat = Category("phones")
+Category("dyson")
+Category("food")
 obj1 = Product("iphone", 234, "...", 3, cat)
 obj2 = Product("lenovo", 32, "...", 5, cat)
 obj3 = Product("samsung", 76, "...", 10, cat)
 
-res = ProductSerializer().serialize_queryset([obj1, obj2, obj3])
 from pprint import pprint
-pprint(res)
+# pprint(product_create())
+pprint(product_list())
+id_ = input("Введите продукт для обновления: ")
+pprint(product_update(id_))
+# pprint(product_list())
+# while True:
+#     id_ = input("Введите id продукта: ")
+#     pprint(product_detail(id_))
